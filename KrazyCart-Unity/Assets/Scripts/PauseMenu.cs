c365c0b7bde3ca1ed8fdc31aa8b9dcc4c2385ab0
nikoxs <nikolaka117@hotmail.com>
2024-02-07 23:17:32 -0600

    added pause menu script

--- KrazyCart-Unity/Assets/Scripts/PauseMenu.cs
+++ KrazyCart-Unity/Assets/Scripts/PauseMenu.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject PausePanel;
+    public static bool paused = false;
+    public GameObject PauseMenuCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Play();
+            }
+            else
+            {
+                Stop();
+            }
+        }
     }
+<<<<<<< Updated upstream
     //EA- I changed the code a little since we aren't able to
     // click the button due to the camera
     public void Pause()
@@ -36,3 +56,25 @@ public class PauseMenu : MonoBehaviour
         //Time.timeScale = 1;
     //}
 }
+=======
+
+    void Stop()
+    {
+        PauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void Play()
+    {
+        PauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    void MainMenuButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
+>>>>>>> Stashed changes
