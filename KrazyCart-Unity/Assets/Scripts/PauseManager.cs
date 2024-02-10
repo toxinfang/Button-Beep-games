c7026f3fa4074280cdf4bf090b707e0108fc7b5b
Mario Barrera <mbavenger2004@gmail.com>
2024-02-10 13:00:28 -0600

    Updated the Pause Script
    It'll play a sound effect after pause or unpausing. Also you can display a background image as well.

--- KrazyCart-Unity/Assets/Scripts/PauseManager.cs
+++ KrazyCart-Unity/Assets/Scripts/PauseManager.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;// Ignore error, it still works.
 public class PauseManager : MonoBehaviour
 {
     public bool isPaused = false;
+    public GameObject backGround;// This is for the Pause Menu Background
+    public TextMeshProUGUI text;// Ignore error, it still works.
+    public AudioSource soundClip;
+    public AudioClip soundEffect;
+    public ThirdPersonControllerM controller;// Upon pausing, should prevent player from moving, as well as pausing camera movement.
 
     // Start is called before the first frame update
     void Start()
     {
-
+        text.text = ("");// Deactivate both text and background upon starting the level.
+        backGround.SetActive(false);
+        Time.timeScale = 1f;// Ensures the game isn't pause apon starting.
     }
 
     // Update is called once per frame
@@ -18,6 +26,7 @@ public class PauseManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P)){
             // Toggle pause state
             TogglePause();
+            soundClip.PlayOneShot(soundEffect); // Plays the sound effect after pressing the Pause key
         }
     }
 
@@ -29,14 +38,18 @@ public class PauseManager : MonoBehaviour
         // If the game is paused, freeze time
         if (isPaused)
         {
-            Debug.Log("Paused");
             Time.timeScale = 0f;
+            text.text = ("Paused");// Can be change to say something else.
+            backGround.SetActive(true);// Can make the background appear upon pausing.
+            controller.canMove = false;// Pauses player and camera.
         }
         else
         {
             // If the game is unpaused, resume time
-            Debug.Log("Unpaused");
             Time.timeScale = 1f;
+            text.text = ("");// Make the text disappear after unpausing.
+            backGround.SetActive(false);// Make the background disappear after unpausing.
+            controller.canMove = true; ;// Unpauses player and camera.
         }
     }
 }
---
+++ KrazyCart-Unity/Assets/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //This gives us access to our UI elements so we can display the text. Only using it internally? Don't need it.
+using TMPro;
+
+//This is a basic timer good for counting down seconds.
+//It counts down as full seconds and stops immediately when you get to 0
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 30f;
+    public float endTime = 0f;
+    public bool timerRunning = false;
+    public TextMeshProUGUI timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerRunning)
+        {
+            if (timeRemaining > endTime)
+            {
+                timeRemaining -= Time.deltaTime;
+                timerText.text = ("Time: ") + Mathf.FloorToInt(timeRemaining + 1).ToString();
+            }
+            else
+            {
+                timerRunning = false;
+                timeRemaining = 0;
+                timerText.text = ("Time: ") + Mathf.FloorToInt(timeRemaining).ToString();
+            }
+        }
+
+        
+    }
+}
